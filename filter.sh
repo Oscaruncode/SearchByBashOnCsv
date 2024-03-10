0a37fefc8fe626d66a3043c0d77ea8884f11273d
oscaruncode <oscarfuentesperezosp@gmail.com>
2024-03-09 19:52:21 -0500

    Added query by all info, and, solved bugs

--- filter.sh
+++ filter.sh
@@ -1,14 +1,26 @@
 #!/bin/bash	
 
-sections=("TITLE" "SUMMARY" "KEYWORDS" "MAIN_OBJETIVE" "METHOD" "RESULTS" "CONCLUSIONS" "RELEVANCE" "MULTISECTIONS")
-regex=("[Cc][Oo]2" "*Amine*" "3" "4" "5" "6" "7" "8" "Custom")
+sections=("TITLE" "SUMMARY" "KEYWORDS" "MAIN_OBJETIVE" "METHOD" "RESULTS" "CONCLUSIONS" "RELEVANCE" "MULTISECTIONS" "ALL")
+regex=("[Cc][Oo]2" "[Aa][Mm][Ii][Nn]" "[Nn][Aa][Nn][Oo]" "[Ii][Oo][Nn][Ii][Cc]" "[Mm][Ii][Nn][Ee][Rr]" "[Cc][Aa][Pp][Tt][Uu][Rr]" "[Aa][Bb][Ss][Oo][Rr]" "[[Ff][Ll][Uu][Ii]" "Custom" "[Mm][Ee][Mm]")
 
+#$TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
 
-
-#Functions
-PRINTSECTION_FUNCT(){  #MODIFY
-	echo $SECTION_RTN
-        #echo SECTIONRTN + CONTENT
+#Function show return section in case that match info // **PENDING*** Pending replace this for pull into a file.txt with the results of the query
+PRINTSECTION_FUNCT(){  
+	echo $SECTION_RTRN
+	 case $SECTION_RTRN in
+        ${sections[0]}) echo "$TITLE" ;;
+          ${sections[1]}) echo "$SUMMARY" ;;
+            ${sections[2]}) echo "$KEYWORDS" ;;
+              ${sections[3]}) echo "$MAIN_OBJETIVE" ;;
+                ${sections[4]}) echo "$METHOD" ;;
+                  ${sections[5]}) echo "$RESULTS" ;;
+                    ${sections[6]}) echo "$CONCLUSIONS" ;;
+                      ${sections[7]}) echo "$RELEVANCE" ;;
+                        ${sections[8]}) echo "MULTI-RETURN" ;; #**PENDING***
+                         ${sections[9]}) echo "$ALLTEXT";;
+               		*) echo "Non-Case" ;;
+	esac
 }
 
 
@@ -22,7 +34,7 @@ done
 read SECTION_NUM
 
 if [[ $SECTION_NUM -eq 9 ]]; then
-    echo "Insert the multi sections (Pending implement)"
+    echo "Insert the multi sections (Pending implement)"  #**PENDING***
     read SECTION
 else
     SECTION="${sections[SECTION_NUM-1]}"
@@ -58,54 +70,62 @@ else
     PATRON="${regex[REGEX_NUM-1]}"
 fi
 
-#Print Selections
+#Print options selected
 echo "Patron $PATRON | section for search $SECTION | section for return $SECTION_RTRN"
 
 #Search
-cat text.csv | while IFS="," read -r TITLE SUMMARY KEYWORDS MAIN_OBJETIVE METHOD RESULTS CONCLUSIONS RELEVANCE; do
+cat archivo.csv | while IFS="," read -r TITLE SUMMARY KEYWORDS MAIN_OBJETIVE METHOD RESULTS CONCLUSIONS RELEVANCE; do
+ALLTEXT="$TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE"
     case $SECTION in
-        ${sections[0]}) #Modify if condition
+        ${sections[0]}) #Could be implement the place where program match the Info with grep  **PENDING***   ALSO CAN REPLACE IF WITH A GREP 
             if [[ $TITLE =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[1]})
             if [[ $SUMMARY =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[2]})
             if [[ $KEYWORDS =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[3]})
             if [[ $MAIN_OBJETIVE =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[4]})
             if [[ $METHOD =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[5]})
             if [[ $RESULTS =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[6]})
             if [[ $CONCLUSIONS =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[7]})
             if [[ $RELEVANCE =~ $PATRON ]]; then
-                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE
+                PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEXT
             fi
             ;;
         ${sections[8]})
             #CUSTOM SEARCH PENDING
+            ;;
+         ${sections[9]})
+            #Search in all the article info (line of csv)
+		  if [[ $ALLTEXT =~ $PATRON ]]; then
+		  	echo $ALLTEXT | grep --color=always $PATRON
+                	 PRINTSECTION_FUNCT $SECTION_RTRN $TITLE $SUMMARY $KEYWORDS $MAIN_OBJETIVE $METHOD $RESULTS $CONCLUSIONS $RELEVANCE $ALLTEX
+            fi           
             ;;
         *)
             echo "No section"
